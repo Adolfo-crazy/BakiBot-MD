68cce117bcac59f62651e94389139ab0f3288b17
ADOLFO SAN <104970935+Adolfo-crazy@users.noreply.github.com>
2022-07-12 02:07:39 -0500

    nv-Que hora son.js

--- plugins/nv-Que hora son.js
+++ plugins/nv-Que hora son.js
@@ -3,13 +3,13 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/Que hora es.mp3'
-conn.sendFile(m.chat, vn, 'Que hora es.mp3', null, m, true, {
+let vn = './media/Que hora son.mp3'
+conn.sendFile(m.chat, vn, 'Que hora son.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /Que hora es|A que hora son/
+handler.customPrefix = /Que hora son/
 handler.command = new RegExp
 export default handler
 
