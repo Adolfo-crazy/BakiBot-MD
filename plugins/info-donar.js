195d41f0bf4b242e861dd0039524d6ec17680aa5
ADOLFO SAN <104970935+Adolfo-crazy@users.noreply.github.com>
2022-07-14 18:10:54 -0500

    info-donar.js

--- plugins/info-donar.js
+++ plugins/info-donar.js
@@ -3,9 +3,9 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
 let name = await conn.getName(m.sender)
 let donar =`
-*MI AMIGO XD NO TENGO PAYPAL*
+*AUN NO TENGO PAYPAL PERO INTENTARE ABRIRLO*
 `.trim()
-conn.sendHydrated(m.chat, donar, wm, null, 'https://www.paypal.me/TheShadowBrokers133', '𝙿𝙰𝚈𝙿𝙰𝙻', null, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
+conn.sendHydrated(m.chat, donar, wm, null, 'https://www.paypal.me/Adolfosenpai', '𝙿𝙰𝚈𝙿𝙰𝙻', null, null, [['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']], m)}
 handler.help = ['donasi']
 handler.tags = ['info']
 handler.command = /^dona(te|si)|donar|apoyar$/i
