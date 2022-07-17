823e74534411a4572dedced8204e13c060ffaef0
ADOLFO SAN <104970935+Adolfo-crazy@users.noreply.github.com>
2022-07-17 02:47:01 -0500

    nv-Callate puto.js

--- plugins/nv-Callate puto.js
+++ plugins/nv-Callate puto.js
@@ -3,12 +3,12 @@ import path from 'path'
 
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
-let vn = './media/bbinario1.txt'
-conn.sendFile(m.chat, vn, 'binario1.txt', null, m, true, {
+let vn = './media/Callate.mp3'
+conn.sendFile(m.chat, vn, 'Callate puto.mp3', null, m, true, {
 type: 'audioMessage', 
 ptt: true 
 })
 }
-handler.customPrefix = /binario1/
+handler.customPrefix = /Callate puto/
 handler.command = new RegExp
 export default handler
